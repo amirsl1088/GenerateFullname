4055487c0c8ae740376517f66f01392d8a18ec14
amir <salamatamir123@gmail.com>
2024-01-12 10:14:49 -0800

    first commit

---
+++ fullname/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fullname
+{
+   public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GetFullName(string firstname,string lastname)
+        {
+            return firstname + " " + lastname;
+        }
+    }
+}
---
+++ fullname/Program.cs
@@ -0,0 +1,9 @@
+﻿using fullname;
+
+Console.WriteLine("enter first name");
+var firstName = Console.ReadLine()!;
+Console.WriteLine("enter last name");
+var lastName = Console.ReadLine()!;
+var p1 = new Person();
+var fullName = p1.GetFullName(firstName, lastName);
+Console.WriteLine($"fullname:{fullName}");
